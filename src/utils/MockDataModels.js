b32af7ac1707cc30214f952e3cb78ee4e44a10dd
Hristo Oskov <hristo.oskov@gmail.com>
2018-11-07 18:04:37 -0800

    update AssociationType when schema updates

--- src/utils/MockDataModels.js
+++ src/utils/MockDataModels.js
@@ -19,11 +19,14 @@ const {
   PropertyTypeBuilder,
 } = Models;
 
+const MOCK_ASSOCIATION_TYPE_FQN :FullyQualifiedName = new FullyQualifiedName('OpenLattice', 'MockAssociationType');
+const MOCK_ENTITY_TYPE_FQN :FullyQualifiedName = new FullyQualifiedName('OpenLattice', 'MockEntityType');
+const MOCK_PROPERTY_TYPE_FQN :FullyQualifiedName = new FullyQualifiedName('OpenLattice', 'MockPropertyType');
 const MOCK_SCHEMA_FQN :FullyQualifiedName = new FullyQualifiedName('OpenLattice', 'MockSchema');
 
 const MOCK_ENTITY_TYPE :EntityType = new EntityTypeBuilder()
   .setId('ec6865e6-e60e-424b-a071-6a9c1603d735')
-  .setType({ namespace: 'OpenLattice', name: 'MockType' })
+  .setType(MOCK_ENTITY_TYPE_FQN)
   .setTitle('title')
   .setDescription('description')
   .setKey([
@@ -42,7 +45,7 @@ const MOCK_ENTITY_TYPE :EntityType = new EntityTypeBuilder()
 
 const MOCK_ENTITY_TYPE_JSON = {
   id: 'ec6865e6-e60e-424b-a071-6a9c1603d735',
-  type: { namespace: 'OpenLattice', name: 'MockType' },
+  type: MOCK_ENTITY_TYPE_FQN.toObject(),
   title: 'title',
   description: 'description',
   key: [
@@ -61,7 +64,7 @@ const MOCK_ENTITY_TYPE_JSON = {
 
 const MOCK_PROPERTY_TYPE :PropertyType = new PropertyTypeBuilder()
   .setId('ec6865e6-e60e-424b-a071-6a9c1603d735')
-  .setType({ namespace: 'OpenLattice', name: 'MockType' })
+  .setType(MOCK_PROPERTY_TYPE_FQN)
   .setTitle('title')
   .setDescription('description')
   .setDataType('String')
@@ -72,7 +75,7 @@ const MOCK_PROPERTY_TYPE :PropertyType = new PropertyTypeBuilder()
 
 const MOCK_PROPERTY_TYPE_JSON = {
   id: 'ec6865e6-e60e-424b-a071-6a9c1603d735',
-  type: { namespace: 'OpenLattice', name: 'MockType' },
+  type: MOCK_PROPERTY_TYPE_FQN.toObject(),
   title: 'title',
   description: 'description',
   datatype: 'String',
@@ -81,8 +84,27 @@ const MOCK_PROPERTY_TYPE_JSON = {
   schemas: [MOCK_SCHEMA_FQN.toObject()],
 };
 
+const MOCK_ASSOCIATION_ENTITY_TYPE :EntityType = new EntityTypeBuilder()
+  .setId('ec6865e6-e60e-424b-a071-6a9c1603d735')
+  .setType(MOCK_ASSOCIATION_TYPE_FQN)
+  .setTitle('title')
+  .setDescription('description')
+  .setKey([
+    '0c8be4b7-0bd5-4dd1-a623-da78871c9d0e',
+    '4b08e1f9-4a00-4169-92ea-10e377070220'
+  ])
+  .setPropertyTypes([
+    '8f79e123-3411-4099-a41f-88e5d22d0e8d',
+    'e39dfdfa-a3e6-4f1f-b54b-646a723c3085',
+    'fae6af98-2675-45bd-9a5b-1619a87235a8'
+  ])
+  .setBaseType('9a768c9b-b76f-4fa1-be60-0178695cdbc3')
+  .setCategory(SecurableTypes.AssociationType)
+  .setSchemas([MOCK_SCHEMA_FQN])
+  .build();
+
 const MOCK_ASSOCIATION_TYPE :AssociationType = new AssociationTypeBuilder()
-  .setEntityType(MOCK_ENTITY_TYPE)
+  .setEntityType(MOCK_ASSOCIATION_ENTITY_TYPE)
   .setSourceEntityTypeIds(['c49832e9-8c49-4d24-984a-2221b4fa249b'])
   .setDestinationEntityTypeIds(['91385fae-babc-4bd3-ba42-74decb9036f0'])
   .setBidirectional(false)
